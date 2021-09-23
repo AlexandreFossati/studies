02bfc5bb7f812bb9e51aef72cd762af05c1841ab
AlexandreFossati <alexandrefossatifilho@gmail.com>
2021-09-23 09:39:36 -0300

    createEntry on controller

--- webapp/controller/ODataModel.controller.js
+++ webapp/controller/ODataModel.controller.js
@@ -9,13 +9,12 @@ sap.ui.define([
 	function (Controller, ODataModel, Fragment) {
 		"use strict";
 
-		return Controller.extend("namespace.studies.controller.ODataModel", { 
-            
-            model: new ODataModel("/Northwind/V2/(S(jesfawywpz3epp00v2x2cvp0))/OData/OData.svc/"),
+		return Controller.extend("namespace.studies.controller.ODataModel", {            
             createDialog: undefined,
             
 			onInit: function () {
-                this.getView().setModel(this.model)
+                const model = new ODataModel("/Northwind/V2/(S(jesfawywpz3epp00v2x2cvp0))/OData/OData.svc/") 
+                this.getView().setModel(model)
 			},
 
             openCreateDialog: function() {
@@ -24,16 +23,49 @@ sap.ui.define([
                 if (!this.createDialog) {
                     this.createDialog = Fragment.load({
                         id: view.getId(),
-                        name: "namespace.studies.view.fragments.ODataModel_CreateDialog"
+                        name: "namespace.studies.view.fragments.ODataModel_CreateDialog",
+                        controller: this
                     }).then(dialog => {
                         view.addDependent(dialog)
                         return dialog
                     })
                 } 
 
-                this.createDialog.then(function(dialog) {
-                    dialog.open()
-                })
+                this.createDialog.then(dialog => dialog.open())
+            },
+
+            onCancelCreate: async function() {
+                try {
+                    const createDialog = await this.createDialog
+                    this.byId("productID").setValue()
+                    this.byId("productRating").setValue()
+                    createDialog.close()
+                }
+                catch(exception) {
+                    console.error(exception)
+                }
+            },
+
+            onConfirmCreate: async function() {
+                try {
+                    const createDialog = await this.createDialog
+                    const productID = this.byId("productID").getValue()
+                    const productRating = this.byId("productRating").getValue()
+                    
+                    const model = this.getView().getModel()
+
+                    model.createEntry(`/Products(${productID})`, {
+                        ID: productID,
+                        Rating: productRating
+                    })
+
+                    model.submitChanges()
+
+                    createDialog.close()
+                }
+                catch(exception) {
+                    console.error(exception)
+                }
             }
 		});
 	});
