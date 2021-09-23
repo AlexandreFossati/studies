4aa894ceeae559e583b38fc0b47f0d7d5baa2c00
AlexandreFossati <alexandrefossatifilho@gmail.com>
2021-09-23 09:11:30 -0300

    creating odatamodel view and fragment layout

--- webapp/controller/ODataModel.controller.js
+++ webapp/controller/ODataModel.controller.js
@@ -1,14 +1,39 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/odata/v2/ODataModel",
+	"sap/ui/core/Fragment"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (Controller, ODataModel, Fragment) {
 		"use strict";
 
-		return Controller.extend("namespace.studies.controller.ODataModel", {
-			
+		return Controller.extend("namespace.studies.controller.ODataModel", { 
             
+            model: new ODataModel("/Northwind/V2/(S(jesfawywpz3epp00v2x2cvp0))/OData/OData.svc/"),
+            createDialog: undefined,
+            
+			onInit: function () {
+                this.getView().setModel(this.model)
+			},
+
+            openCreateDialog: function() {
+                var view = this.getView()
+
+                if (!this.createDialog) {
+                    this.createDialog = Fragment.load({
+                        id: view.getId(),
+                        name: "namespace.studies.view.fragments.ODataModel_CreateDialog"
+                    }).then(dialog => {
+                        view.addDependent(dialog)
+                        return dialog
+                    })
+                } 
+
+                this.createDialog.then(function(dialog) {
+                    dialog.open()
+                })
+            }
 		});
 	});
