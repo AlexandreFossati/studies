0e245b057085ce010af5b04d3eae305bb917ca3f
AlexandreFossati <alexandrefossatifilho@gmail.com>
2021-09-24 19:56:51 -0300

    navigation list on root control is now dynamic

--- webapp/controller/Root.controller.js
+++ webapp/controller/Root.controller.js
@@ -1,19 +1,35 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (Controller, JSONModel) {
 		"use strict";
 
 		return Controller.extend("namespace.studies.controller.Root", {
+			onInit: function() {
+				const component = this.getOwnerComponent()
+				const router = component.getRouter()
+				const routes = Object.keys(router._oRoutes)
+				
+				const navigationList = routes.map(route => {
+					return { 
+						name: route.replaceAll("_", " "),
+						route: route
+					}
+				})
+
+				this.getView()
+					.setModel(new JSONModel(navigationList), "navigationList")
+			},
 			onSelectItem: function(event) {
 				const selectedKey = event.getSource().getSelectedKey()
 
 				this.getOwnerComponent()
 					.getRouter()
-					.navTo(`Route${selectedKey}`)
+					.navTo(selectedKey)
 			}
 		});
 	});
